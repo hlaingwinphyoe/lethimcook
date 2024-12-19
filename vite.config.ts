e141a8923c11c89745654a57c7db827f47e3cc4c
hlaingwinphyoe <dev.hlaingwinphyoe@gmail.com>
2024-12-19 11:32:46 +0700

    version-1

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,20 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+        chunkSizeWarningLimit: 1600,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return id
+                            .toString()
+                            .split("node_modules/")[1]
+                            .split("/")[0]
+                            .toString();
+                    }
+                },
+            },
+        },
+    },
 });
